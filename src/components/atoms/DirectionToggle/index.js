e46c6bcb300507f023a8085491b81134cc47f036
gun2 <0dayz@naver.com>
2022-07-21 01:22:59 +0900

    수정(방향토글): 내부 처리 로직 리듀서로 변경.

--- src/components/atoms/DirectionToggle/index.js
+++ src/components/atoms/DirectionToggle/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useReducer } from 'react';
+import React, {useCallback, useEffect, useReducer} from 'react';
 import { MdOutlineArrowForwardIos } from 'react-icons/md';
 import styled from 'styled-components';
 
@@ -16,17 +16,22 @@ const Button = styled.div`
     };
 `;
 
-function increaseCount(state){
-    return state + 1
+function reducer(state,{type}){
+    switch (type){
+        case "INCREASE":
+            return state+1;
+        case "INIT":
+            return 1;
+    }
 }
 
 function DirectionToggle({degree = 0, onToggle}) {
     //console.log(Box);
-    const [count, increaseDispatch] = useReducer(increaseCount, 2)
-    const onClick = useCallback(() => {
-        increaseDispatch();
+    const [count, increaseDispatch] = useReducer(reducer, 0)
+    const onClick = () => {
+        increaseDispatch({type:"INCREASE"});
         onToggle(count % 2);
-    })
+    }
     return (
         <Box style={{
             transform : `rotate(${((count % 2) * 180) + degree}deg
