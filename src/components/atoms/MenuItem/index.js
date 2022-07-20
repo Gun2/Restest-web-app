b7f54494d7c6796210a719fa0c878e35b634eda5
gun2 <0dayz@naver.com>
2022-07-21 01:24:49 +0900

    추가(메뉴아이템): 메뉴 아이템 컴포넌트 추가.

--- src/components/atoms/MenuItem/index.js
+++ src/components/atoms/MenuItem/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import styled, {css} from 'styled-components';
-import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import {NavLink} from 'react-router-dom';
+
 MenuItem.propTypes = {
     
 };
